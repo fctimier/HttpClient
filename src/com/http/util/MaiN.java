8745c947d24714c3016ef12c79ef9a7c908feedf
fctimier <qq1098013518@163.com>
2021-03-10 17:59:32 +0800

    添加依赖后测试成功。

--- src/com/http/util/MaiN.java
+++ src/com/http/util/MaiN.java
@@ -1,5 +1,15 @@
 package com.http.util;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * TODO
  *
@@ -14,6 +24,24 @@ public class MaiN {
         HttpUtil httpUtil = new HttpUtil();
 
         String rs = httpUtil.doGet(url, "");
+
         System.out.println(rs);
+
+        /*String result;
+        HttpGet get = new HttpGet(url);
+        DefaultHttpClient client = new DefaultHttpClient();
+        try {
+            HttpResponse response = client.execute(get);
+            result = EntityUtils.toString(response.getEntity());
+
+            CookieStore cookieStore =   client.getCookieStore();
+            List<Cookie> cookies =cookieStore.getCookies();
+            System.out.println(cookies.size());
+            for(Cookie c :cookies) {//get Cookies info
+                System.out.println(c.getName() + ":" + c.getValue());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        }
     }
-}
